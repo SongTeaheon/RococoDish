31b9d305bf32ccca09c012d0199510f838eed6c3
hdg04 <hdg0430@naver.com>
2019-05-11 19:32:21 +0900

    일요일 전 마지막 심플한 틀 완성

---
+++ app/src/main/java/StorePage.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.front_ui.R;
+
+public class StorePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_page);
+
+    }
+}
--- app/src/main/java/com/example/front_ui/DishView.java
+++ app/src/main/java/com/example/front_ui/DishView.java
@@ -1,19 +1,26 @@
 package com.example.front_ui;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class DishView extends AppCompatActivity {
 
     Button buttonToDetail;
+    ImageView imageView;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dish_view);
+
+        buttonToDetail = (Button) findViewById(R.id.toDetail1);
+        imageView = (ImageView) findViewById(R.id.imageView1);
 
         buttonToDetail.setOnClickListener(new View.OnClickListener() {
             @Override
