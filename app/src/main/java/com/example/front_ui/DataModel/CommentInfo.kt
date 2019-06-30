edec2eb6b0f482e3184247f9565a4b90dffeacdd
Taewan Kim <terry960302>
2019-07-01 01:37:09 +0900

    [좋아요 디비 에러 수정완료]+[질문기능 완료(수정, 삭제는 불가)]+[태그, 게시물 내용 띄우기(태그는 수정필요)]

--- app/src/main/java/com/example/front_ui/DataModel/CommentInfo.kt
+++ app/src/main/java/com/example/front_ui/DataModel/CommentInfo.kt
@@ -2,9 +2,10 @@ package com.example.front_ui.DataModel
 
 import java.sql.Timestamp
 
-class CommentInfo(val imgPath : String,
+class CommentInfo(val commenWriterId : String,
+                  val imgPath : String?,
                   val question : String,
-                  val answer : String,
+                  val answer : String?,
                   val time : Long) {
-    constructor(): this("", "", "",0)
+    constructor(): this("","", "", null,0)
 }
