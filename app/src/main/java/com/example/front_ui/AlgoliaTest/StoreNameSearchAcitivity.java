a15f190f35b112c07002c6f1a3d00031b2698505
SongTeaheon <sth534@gmail.com>
2019-07-07 15:18:01 +0900

    임시 커밋 Algolia test만드는 중

---
+++ app/src/main/java/com/example/front_ui/AlgoliaTest/StoreNameSearchAcitivity.java
@@ -0,0 +1,15 @@
+package com.example.front_ui.AlgoliaTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.front_ui.R;
+
+public class StoreNameSearchAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_name_search_acitivity);
+    }
+}
