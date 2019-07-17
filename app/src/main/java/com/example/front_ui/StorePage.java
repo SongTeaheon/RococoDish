dd42cd0e8ed0d5ecfaf530e5ff03637fc9482436
Taewan Kim <terry960302>
2019-07-17 17:07:08 +0900

    [가게 페이지 데이터 연동]+[좋아요 개수 디비에 게시물 진입 시 업로드]

--- app/src/main/java/com/example/front_ui/StorePage.java
+++ app/src/main/java/com/example/front_ui/StorePage.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.TextView;
 
+import com.example.front_ui.DataModel.StoreInfo;
+import com.example.front_ui.DataModel.StorePostInfo;
 import com.example.front_ui.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StorePage extends AppCompatActivity {
 
@@ -18,6 +28,9 @@ public class StorePage extends AppCompatActivity {
 
     TextView tv_storeName;
     TextView tv_storeStar;
+    GridView gridView;
+    List<StorePostInfo> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +42,35 @@ public class StorePage extends AppCompatActivity {
         docId = intent.getStringExtra("documentId");
 
         Log.d(TAG, "name, star, id :" + storeName + storeStar + docId);
-        tv_storeName = findViewById(R.id.storeName);
-        tv_storeStar = findViewById(R.id.storeStar);
+        tv_storeName = findViewById(R.id.tvStore);
 
         tv_storeName.setText(storeName);
-        tv_storeName.setText(storeStar);
 
-        //가게 데이터 넣는 코드 필요!!
+        //그리드 뷰 세팅
+        gridView = findViewById(R.id.gridView0);
+        final StorePageAdapter storePageAdapter = new StorePageAdapter(this, list);
+        gridView.setAdapter(storePageAdapter);
 
+        //가게 해당 게시물 가져오기
+        FirebaseFirestore.getInstance()
+                .collection("포스팅")
+                .whereEqualTo("storeId", docId)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(DocumentSnapshot dc : queryDocumentSnapshots.getDocuments()){
+
+                            //TODO : postingInfo와 StoreInfo 둘다 가져와야 클릭해서 DishView들어갈 수 있음.
+                            String image = dc.getData().get("imagePathInStorage").toString();
+                            String numLike =  dc.getData().get("numLike").toString();
+                            String numStar = dc.getData().get("aver_star").toString();
+
+                            list.add(new StorePostInfo(image, numLike, numStar));
+                            storePageAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
 
     }
 }
