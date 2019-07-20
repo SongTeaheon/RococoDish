cdfca5f08ac3d64dcf4bf09c5b7575686a3c5b33
Taewan Kim <terry960302>
2019-07-20 11:55:08 +0900

    [이스터에그]+[마이페이지 비동기 패치]+[가게페이지 중복 오류 수정]

---
+++ app/src/main/java/com/example/front_ui/SettingActivity_Dialog.kt
@@ -0,0 +1,29 @@
+package com.example.front_ui
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.dialog_setting.*
+
+class SettingActivity_Dialog(context : Context) : Dialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //다이얼로그 밖의 화면을 흐리게
+        val layoutParams = WindowManager.LayoutParams()
+        layoutParams.apply {
+            this.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
+            this.dimAmount = 0.8f
+        }
+        window.attributes = layoutParams
+
+        setContentView(R.layout.dialog_setting)
+
+        button.setOnClickListener {
+            dismiss()
+        }
+
+    }
+}
--- app/src/main/java/com/example/front_ui/StorePageActivity.java
+++ app/src/main/java/com/example/front_ui/StorePageActivity.java
@@ -9,17 +9,14 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import com.example.front_ui.DataModel.PostingInfo;
 import com.example.front_ui.DataModel.StoreInfo;
 import com.example.front_ui.DataModel.StorePostInfo;
-import com.example.front_ui.R;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
@@ -40,6 +37,7 @@ public class StorePageActivity extends AppCompatActivity {
     GridView gridView;
     ProgressBar progressBar;
     List<StorePostInfo> list = new ArrayList<>();
+    StoreInfo storeInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +72,21 @@ public class StorePageActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
         //가게 해당 게시물 가져오기
         //TODO : 시간순으로 정렬하는거 해결하기
+
+        //가게 도큐먼트로 가서 객체로 가져와야함.
+        FirebaseFirestore.getInstance()
+                .collection("가게")
+                .document(docId)
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                        if(e != null)
+                            Log.d(TAG, e.getMessage());
+                        assert documentSnapshot != null && documentSnapshot.exists();
+                        storeInfo = documentSnapshot.toObject(StoreInfo.class);
+                    }
+                });
+
         FirebaseFirestore.getInstance()
                 .collection("포스팅")
                 .whereEqualTo("storeId", docId)
@@ -82,32 +95,39 @@ public class StorePageActivity extends AppCompatActivity {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                         if(!queryDocumentSnapshots.getDocuments().isEmpty()){
-                            for(DocumentSnapshot dc : queryDocumentSnapshots.getDocuments()){
+                            for(DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()){
+                                if(dc.getType() == DocumentChange.Type.ADDED){
+                                    //게시물의 이미지, 별점, 좋아요 개수가져옴.
+                                    final String image = dc.getDocument().getData().get("imagePathInStorage").toString();
+                                    final String numLike =  dc.getDocument().getData().get("numLike").toString();
+                                    final String numStar = dc.getDocument().getData().get("aver_star").toString();
 
-                                //게시물의 이미지, 별점, 좋아요 개수가져옴.
-                                final String image = dc.getData().get("imagePathInStorage").toString();
-                                final String numLike =  dc.getData().get("numLike").toString();
-                                final String numStar = dc.getData().get("aver_star").toString();
+                                    final PostingInfo postingInfo = dc.getDocument().toObject(PostingInfo.class);
 
-                                final PostingInfo postingInfo = dc.toObject(PostingInfo.class);
-
-                                //가게 도큐먼트로 가서 객체로 가져와야함.
-                                FirebaseFirestore.getInstance()
-                                        .collection("가게")
-                                        .document(docId)
-                                        .get()
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                                StoreInfo storeInfo = documentSnapshot.toObject(StoreInfo.class);
-
-                                                list.add(new StorePostInfo(image, numLike, numStar, postingInfo, storeInfo));
-                                                storePageAdapter.notifyDataSetChanged();
-                                                progressBar.setVisibility(View.GONE);
-                                                gridView.setVisibility(View.VISIBLE);
-                                            }
-                                        });
+                                    list.add(new StorePostInfo(image, numLike, numStar, postingInfo, storeInfo));
+                                }
                             }
+                            storePageAdapter.notifyDataSetChanged();
+                            progressBar.setVisibility(View.GONE);
+                            gridView.setVisibility(View.VISIBLE);
+//                            for(DocumentSnapshot dc : queryDocumentSnapshots.getDocuments()){
+//
+//                                //게시물의 이미지, 별점, 좋아요 개수가져옴.
+//                                final String image = dc.getData().get("imagePathInStorage").toString();
+//                                final String numLike =  dc.getData().get("numLike").toString();
+//                                final String numStar = dc.getData().get("aver_star").toString();
+//
+//                                final PostingInfo postingInfo = dc.toObject(PostingInfo.class);
+//
+//                                list.add(new StorePostInfo(image, numLike, numStar, postingInfo, storeInfo));
+//
+//                                storePageAdapter.notifyDataSetChanged();
+//                                progressBar.setVisibility(View.GONE);
+//                                gridView.setVisibility(View.VISIBLE);
+//
+//                            }
+
+
                         }
                         else{
                             Log.d(TAG, "해당 가게에 게시물이 없습니다.");
