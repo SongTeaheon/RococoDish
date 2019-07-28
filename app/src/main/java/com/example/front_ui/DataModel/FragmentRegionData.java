efc4b51ea13eb354a1e3428a6822f3d2a09cda28
hdg04 <hdg0430@naver.com>
2019-07-29 00:06:51 +0900

    일단 빠르게 아이템이랑 리사이클러뷰 4개 검색 페이지에 추가했습니다. 최근검색은 추후 올리겠습니당.

--- app/src/main/java/com/example/front_ui/DataModel/FragmentRegionData.java
+++ app/src/main/java/com/example/front_ui/DataModel/FragmentRegionData.java
@@ -4,19 +4,8 @@ import java.io.Serializable;
 
 public class FragmentRegionData implements Serializable {
 
-    private int imageViewRegion; //일단 빠르게 작성하려고 int로 해놓았으니 String으로 바꿔주세요 ㅜㅜ
     private String itemRegionName;
-    private float itemRegionDistance;
     private String itemRegionAddress;
-    private int itemRegionPosts;
-
-    public int getImageViewRegion() {
-        return imageViewRegion;
-    }
-
-    public void setImageViewRegion(int imageViewRegion) {
-        this.imageViewRegion = imageViewRegion;
-    }
 
     public String getItemRegionName() {
         return itemRegionName;
@@ -26,27 +15,11 @@ public class FragmentRegionData implements Serializable {
         this.itemRegionName = itemRegionName;
     }
 
-    public float getItemRegionDistance() {
-        return itemRegionDistance;
-    }
-
-    public void setItemRegionDistance(float itemRegionDistance) {
-        this.itemRegionDistance = itemRegionDistance;
-    }
-
     public String getItemRegionAddress() {
         return itemRegionAddress;
     }
 
     public void setItemRegionAddress(String itemRegionAddress) {
         this.itemRegionAddress = itemRegionAddress;
-    }
-
-    public int getItemRegionPosts() {
-        return itemRegionPosts;
-    }
-
-    public void setItemRegionPosts(int itemRegionPosts) {
-        this.itemRegionPosts = itemRegionPosts;
     }
 }
--- app/src/main/java/com/example/front_ui/DataModel/FragmentSotreData.java
+++ app/src/main/java/com/example/front_ui/DataModel/FragmentSotreData.java
@@ -5,10 +5,7 @@ import java.io.Serializable;
 public class FragmentSotreData implements Serializable {
 
     private String storeName;
-    private float storeDistance;
     private String storeAddress;
-    private int storePosts;
-    private float storeScore;
 
     public String getStoreName() {
         return storeName;
@@ -18,35 +15,11 @@ public class FragmentSotreData implements Serializable {
         this.storeName = storeName;
     }
 
-    public float getStoreDistance() {
-        return storeDistance;
-    }
-
-    public void setStoreDistance(float storeDistance) {
-        this.storeDistance = storeDistance;
-    }
-
     public String getStoreAddress() {
         return storeAddress;
     }
 
     public void setStoreAddress(String storeAddress) {
         this.storeAddress = storeAddress;
-    }
-
-    public int getStorePosts() {
-        return storePosts;
-    }
-
-    public void setStorePosts(int storePosts) {
-        this.storePosts = storePosts;
-    }
-
-    public float getStoreScore() {
-        return storeScore;
-    }
-
-    public void setStoreScore(float storeScore) {
-        this.storeScore = storeScore;
     }
 }
