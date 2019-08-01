513248b0be511856573c25b81fc494c899c57dac
Taewan Kim <terry960302>
2019-08-01 15:44:48 +0900

    [로그인 ui수정] + [대댓글 말고 댓글 클릭 후 마이페이지 이동]

--- app/src/main/java/com/example/front_ui/SignUpAdapter.java
+++ app/src/main/java/com/example/front_ui/SignUpAdapter.java
@@ -1,22 +1,25 @@
 package com.example.front_ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class SignUpAdapter extends PagerAdapter {
 
-    private int[] images = {R.drawable.illustration, R.drawable.illustration0, R.drawable.illustration2};
+    private int[] images = {R.drawable.rococo_logo2 ,R.drawable.ic_store, R.drawable.ic_food, R.drawable.ic_breakfast};
     private String[] texts = {
+            "로코코디쉬",
             "다른 사람들이 모아둔 식사를 모아보세요. \n 당신의 식사들을 소중히 담아두세요.",
-            "맛있는 식사! \n 퀄리티 좋은 가게! \n 로코코디쉬에서 찾아보세요.",
+            "맛있는 식사 \n 퀄리티 좋은 가게 \n 로코코디쉬에서 찾아보세요.",
             "이제 시작해보세요 \n 로코코디쉬"
     };
     LayoutInflater inflater;
@@ -44,6 +47,20 @@ public class SignUpAdapter extends PagerAdapter {
 
         ImageView image = view.findViewById(R.id.image_activitySignUpPage);
         TextView text = view.findViewById(R.id.text_activitySignUpPage);
+        Button startBtn = view.findViewById(R.id.startBtn_btn_activitySignUpPage);
+
+        if(position != 3){
+            startBtn.setVisibility(View.GONE);
+        }
+        else{
+            startBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, LoginDialog.class);
+                    context.startActivity(intent);
+                }
+            });
+        }
 
         image.setImageResource(images[position]);
         text.setText(texts[position]);
